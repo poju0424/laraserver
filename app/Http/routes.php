c66deb20e44b688de1a1641f8cb356e485212efb
pojulee <poju0424@gmail.com>
2017-03-06 15:09:30 +0800

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Route::get('newest/{currency}', function($currency) {
 	$tableName = "bot_".$currency
-	$data = DB::table($tableName)->get();
-	return $data;
+	$langs = DB::table($tableName)->get();
+	return $langs;
 });
 
