52c448499afb1fbb838a7ef0d127480f145ae98f
pojulee <poju0424@gmail.com>
2017-03-06 14:11:51 +0800

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 
 Route::get('newest/{currency}', function($currency) {
 	// $data = DB::table($currency)->get();
+	$data = DB::connection('default')->select("SELECT * FROM ".$currency."");
 	return $currency;
 });
 
