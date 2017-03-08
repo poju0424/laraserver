1133fd1b02d6065a85e4e2e4170035dbd27485be
pojulee <poju0424@gmail.com>
2017-03-08 15:21:24 +0800

    test

--- app/Http/Controllers/RateController.php
+++ app/Http/Controllers/RateController.php
@@ -49,32 +49,37 @@ class RateController extends BaseController
     public function show($input)
     {
         $find = str_replace(" ","", strtolower($input));
-		$output = 0;
+		$output = -1;
 		$searchList = array(
-			array("日", "jpy"),
-			array("jp", "jpy"),
-			array("美", "usd"),
-			array("us", "usd"),
-			array("人民", "cny"),
-			array("rmb", "cny"),
-			array("cn", "cny"),
-			array("歐", "eur"),
-			array("eu", "eur"),
-			array("港", "hkd"),
-			array("hk", "hkd")
+			array("日", "jpy", "日圓"),
+			array("jp", "jpy", "日圓"),
+			array("美", "usd", "美金"),
+			array("us", "usd", "美金"),
+			array("人民", "cny", "人民幣"),
+			array("rmb", "cny", "人民幣"),
+			array("cn", "cny", "人民幣"),
+			array("歐", "eur", "歐元"),
+			array("eu", "eur", "歐元"),
+			array("港", "hkd", "港幣"),
+			array("hk", "hkd", "港幣")
 		);
 		
 		for ($i=0, $max=count($searchList); $i<$max; $i++) {
 			$pos = strpos($find, $searchList[$i][0]);
 			if ($pos !== false){
-				$output = $searchList[$i][1];
+				$output = $i;
 				break;
 			}
 		}
 		if($output !==0){
-			$tableName = "bot_".$output;
+			$tableName = "bot_".$searchList[$output][1];
 			$data = (array)DB::table($tableName)->orderBy('datetime', 'desc')->first();
-			echo $data["cashbuy"];
+			return "台銀"+$searchList[$output][2]+"即時匯率:".
+					"\n 現金買入:".$data["cashbuy"].
+					"\n 現金賣出:".$data["cashsell"].
+					"\n 即期買入:".$data["ratebuy"].
+					"\n 即期賣出:".$data["ratesell"].
+					"\n 更新時間(".$data["datetime"].")";
 		}else{
 			return "(黑人問號 .jpg)";
 		}
