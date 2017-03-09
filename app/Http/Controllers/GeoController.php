678ba4ffbc913f08f8aaa849784f85d3270b59b6
pojulee <poju0424@gmail.com>
2017-03-09 15:19:47 +0800

    test

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -50,13 +50,9 @@ class GeoController extends BaseController
      */
     public function show($input)
     {
-<<<<<<< HEAD
-        echo "show";
-=======
         $API_key = parse_url(getenv('GOOGLE_GEO_API_KEY'));
 		$client = new Client(); //GuzzleHttp\Client
 		echo "show";
->>>>>>> b1f3e45fbca07f226567edd5c78767681f1bdd84
     }
 
     /**
