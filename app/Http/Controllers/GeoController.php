7a19be2b8d443059867fd05f5ee965d60bd03084
pojulee <poju0424@gmail.com>
2017-03-09 15:32:36 +0800

    tttt

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -52,7 +52,7 @@ class GeoController extends BaseController
 		$Geo_url = "https://maps.googleapis.com/maps/api/geocode/json?address=".$input."&key=".$API_key."";
 		$client = new Client(); //GuzzleHttp\Client
 		$result = $client->get($Geo_url);
-		print_r ($result);
+		echo ($result[["results"]["formatted_address"]);
     }
 
     /**
