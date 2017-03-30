35c8d3eb0b0eed111d5fcb59e95f9df4ec674d39
pojulee <poju0424@gmail.com>
2017-03-30 16:41:34 +0800

    test

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -16,7 +16,19 @@ class GeoController extends BaseController
      */
     public function index()
     {
-        echo "index";
+        // create http client instance
+        $client = new Client('http://download.cloud.com/releases');
+ 
+        // create a request
+        $request = $client->get('/3.0.6/api_3.0.6/TOC_Domain_Admin.html');
+        
+        // send request / get response
+        $response = $request->send();
+        
+        // this is the response body from the requested page (usually html)
+        $result = $response->getBody();
+        
+        echo $result;
     }
 
     /**
